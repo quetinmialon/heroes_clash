901e4f65d1e5971a2adc9c4b5261cb13368633a1
Matgui4 <mathisguillotin@gmail.com>
2023-12-08 11:29:35 +0100

    j'ai fait le pop up de bibliotheque et l'ajout des pages sur le html

---
+++ fichier_JS/fonctionMathis.js
@@ -0,0 +1,26 @@
+import {creerUnElement} from "./fonctionsGlobales.js"
+import {hero} from "./variableGlobale.js"
+
+
+
+let boutonBibliotheque = document.getElementById("Bibliothèque")
+console.log()
+boutonBibliotheque.addEventListener("click", ()=>{
+    let conteneurPrincipal = creerUnElement("conteneurPrincipal_ID","conteneurPrincipal_Class","div",document.body)
+
+    let cP_BoutonSuppr = creerUnElement("cP_BoutonSuppr_ID","cP_BoutonSuppr_Class","div",conteneurPrincipal)
+    cP_BoutonSuppr.addEventListener("click", ()=>{
+        cP_BoutonSuppr.addEventListener("mouseover", ()=>{
+            cP_BoutonSuppr.s
+        })
+
+        conteneurPrincipal.remove()
+        
+    })
+
+    let slot = [1, 2, 3, 4]
+    for(let i of slot){
+        let div_i = document.createElement("div")
+    }
+
+})
