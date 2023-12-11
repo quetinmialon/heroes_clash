d5ca49472725e667b0b0998b29d2ae115ebe30d6
Matgui4 <mathisguillotin@gmail.com>
2023-12-11 09:19:59 +0100

    mathis A JOUR BITCHES  !

--- fichier_JS/fonctionMathis.js
+++ fichier_JS/fonctionMathis.js
@@ -1,36 +1,41 @@
-import { glass, createGlass } from "./fonctionQuentin.JS"
 import { creerUnElement } from "./fonctionsGlobales.js"
-
-
-
-
-
+import { hero } from "./variableGlobale.js"
+import { glass, createGlass } from "./fonctionQuentin.JS"
 
 
 
 let boutonBibliotheque = document.getElementById("Bibliothèque")
 console.log()
 boutonBibliotheque.addEventListener("click", () => {
-    createGlass()
-    let conteneurPrincipal = creerUnElement("conteneurPrincipal_ID", "conteneurPrincipal_Class", "div", glass)
+    //mise en place de la glasse pour pouvoir clicker en dehors de la fenetre pour la fermer
 
-    let cP_BoutonSuppr = creerUnElement("cP_BoutonSuppr_ID", "cP_BoutonSuppr_Class", "div", conteneurPrincipal)
-    cP_BoutonSuppr.addEventListener("click", () => {
-        
+    createGlass()
+    //creation du conteneur principal accueillant les éléments de la bibliothèque
+    let conteneurPrincipal = creerUnElement("conteneurPrincipal_ID", "conteneurPrincipal_Class", "div", glass)
+    let ligne1 = creerUnElement("ligne1", "ligne1", "div", conteneurPrincipal)
+    //création du conteneur du bouton pour supprimer la fenetre
+    let cP_BoutonSuppr = creerUnElement("cP_BoutonSuppr_ID", "cP_BoutonSuppr_Class", "div", ligne1)
+
+    //creation dun conteneur pour y metre une image afin de l'animer au survol
+    let image = creerUnElement("", "image", "img", cP_BoutonSuppr)
+
+    //définition de l'image
+    image.setAttribute("src", "../dossierImage/images.png")
+
+    //Add d'un event au click pour le bouton, ici pour suppr la fenetre et la glass
+    image.addEventListener("click", () => {
         conteneurPrincipal.remove()
         glass.remove()
     })
-    cP_BoutonSuppr.addEventListener("mouseover", function (e) {
-        e.target.setAttribute("class", "cP_B");
-    }
-    )
-    cP_BoutonSuppr.addEventListener("mouseout", function (e) {
-        e.target.setAttribute("class", "cP_BoutonSuppr_Class");
-    }
-    )
-    let slot = [1, 2, 3, 4]
-    for (let i of slot) {
-        let div_i = document.createElement("div")
-    }
+    //add d'un event au survol de l'image pour que celle ci change
+    image.addEventListener("mouseover", function (e) {
+        e.target.setAttribute("src", "../dossierImage/poubelle-ouverte.png");
+    })
+    //add d'un event en enlevant le curseur de l'image pour la remettre à son etat initial
+    image.addEventListener("mouseout", function (e) {
+        e.target.setAttribute("src", "../dossierImage/images.png");
+    })
+
+    
 
 })
